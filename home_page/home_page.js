74f56f6d21c6966c686c46815059791b73576adb
hu <192@qq.com>
2017-08-08 00:34:10 +0800

    更新

--- home_page/home_page.js
+++ home_page/home_page.js
@@ -1,7 +1,6 @@
 (function(angular){
     var app = angular.module("home_page",["ngRoute"]);
     app.config(["$routeProvider",function($routeProvider) {
-        console.log(22);
         $routeProvider.when("/home_page?",{
             template:'<div class="list"><img src="assets/img/home_page.png" alt=""></div>',
             controller:"home_pageController"
